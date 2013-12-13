d63f8e1adf686121f1bdaac49785d3a64035bdd7
Georgiy <goshazvir@gmail.com>
2013-12-13 16:35:01 +0200

    1

--- js/common.js
+++ js/common.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+	$(function() {
+		var footerHeight = $(".footer").height();
+		$(".out").css("margin-bottom", -footerHeight);
+		$(".push").css("height", footerHeight);
+	});
 
+	$(window).resize(function() {
+		var footerHeight = $(".footer").height();
+		$(".out").css("margin-bottom", -footerHeight);
+		$(".push").css("height", footerHeight);
+	});
 
 });
